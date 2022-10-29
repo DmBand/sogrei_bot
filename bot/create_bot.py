564530d332cdeac60138cd2b8ce82dd41f325a17
DmBand <d.bandysik@gmail.com>
2022-10-29 19:31:30 +0300

    add state for ppt_calculator

--- bot/create_bot.py
+++ bot/create_bot.py
@@ -1,6 +1,9 @@
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from secret import TOKEN
 
+storage = MemoryStorage()
+
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
